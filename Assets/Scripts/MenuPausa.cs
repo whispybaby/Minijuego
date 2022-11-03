4637cd23bb9da7b21e71c25cbb35f3d87b0a9e00
Gabriel Barrientos <gabri12.rubik@gmail.com>
2022-11-03 16:31:50 -0300

    Faltó cambiar el nombre de la clase

--- Assets/Scripts/MenuPausa.cs
+++ Assets/Scripts/MenuPausa.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class PauseMenu : MonoBehaviour
+public class MenuPausa : MonoBehaviour
 {
     public Button BotonPrincipal;
     public GameObject PanelOpciones;
